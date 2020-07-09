49ae5e682076db45e7d486551a1c1c03f6cf6cea
DennisVera <dennisvera.z@gmail.com>
2020-07-09 15:34:48 -0400

    Initialize project

---
+++ TinderDemo/Modules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderDemo
+//
+//  Created by Dennis Vera on 7/9/20.
+//  Copyright © 2020 Dennis Vera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
